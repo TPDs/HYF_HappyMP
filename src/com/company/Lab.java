41a5f7cc4cbd270c3c0885c471d08eaf543af94d
TPD <mig.mic@gmail.com>
2020-09-10 14:55:09 +0200

    Maze system up

--- src/com/company/Lab.java
+++ src/com/company/Lab.java
@@ -1,17 +1,13 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Lab {
     int[] hor = new int[13];
     int[] ver = new int[13];
 
     ArrayList<Wall> wall = new ArrayList<>();
-
-    /*public Lab(int[] hor, int[] ver) {
-        this.hor = hor;
-        this.ver = ver;
-    }*/
 
     public void initiateGrid() {
         for(int i = 0; i<=12; i++){
@@ -20,23 +16,18 @@ public class Lab {
         }
     }
 
-    public void initiateWall(){
-        for(int i = 0; i<=11; i++){wall.add(new Wall(0,i)); wall.add(new Wall(12,i));} //The sides
-        for(int j = 0; j<=12; j++){wall.add(new Wall(j,0)); wall.add(new Wall(j,12));} //The bottom and top
-        wall.remove()
+    public void initiateWall(int ind,int ud){
+        for(int i = 0; i<=11; i++){ wall.add(new Wall(0,i)); wall.add(new Wall(12,i));} //The sides
 
-        wall.add(new Wall(0,0));
-        wall.add(new Wall(0,1));
-        wall.add(new Wall(0,2));
-        wall.add(new Wall(0,3));
-        wall.add(new Wall(0,4));
-        wall.add(new Wall(0,5));
-        wall.add(new Wall(0,6));
-        wall.add(new Wall(0,7));
-        wall.add(new Wall(0,8));
-        wall.add(new Wall(0,9));
-        wall.add(new Wall(0,10));
-        wall.add(new Wall(0,11));
-        wall.add(new Wall(0,12));
+        for(int j = 0; j<=12; j++){
+            if (j== ind || j== ud) {continue;}
+            else
+            wall.add(new Wall(j,0)); wall.add(new Wall(j,12));} //The bottom and top
+
+        Scanner sc = new Scanner( "lab.txt");
+         while (sc.hasNextLine())
+         {
+             wall.add(new Wall(sc.nextInt(),sc.nextInt()));
+         }
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Lab start = new  Lab();
+
+        start.initiateGrid();
+        start.initiateWall(3,5);
+
     }
 }
